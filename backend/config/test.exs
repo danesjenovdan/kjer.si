35d8a20f4d62f88b4304bd8c8da158e66fb8690e
mzgajner <mato@zgajner.com>
2019-11-02 10:56:59 +0100

    Use separate db name to run tests

--- backend/config/test.exs
+++ backend/config/test.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :kjer_si, KjerSi.Repo,
   username: System.get_env("PGUSER"),
   password: System.get_env("PGPASSWORD"),
-  database: System.get_env("PGDATABASE"),
+  database: System.get_env("PGDATABASE") <> "_test",
   hostname: System.get_env("PGHOST"),
   pool: Ecto.Adapters.SQL.Sandbox
 
