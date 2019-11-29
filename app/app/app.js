8ac33856708049538641960db9c699cea3bb5d53
FranciZ <franci@proxima.si>
2019-11-29 20:38:42 +0100

    Setup Google Maps plugin.

--- app/app/app.js
+++ app/app/app.js
@@ -1,6 +1,12 @@
 import Vue from "nativescript-vue";
-
+import {Mapbox} from 'nativescript-mapbox'
 import Home from "./components/Home/Home";
+import { MapView } from "nativescript-google-maps-sdk";
+
+Vue.config.silent = false;
+
+Vue.registerElement('MapView', () => MapView);
+Vue.registerElement('MapBox', () => Mapbox);
 
 new Vue({
 
--- app/app/components/Discover/Discover.vue
+++ app/app/components/Discover/Discover.vue
@@ -6,11 +6,36 @@
 </style>
 
 <script>
-    export default {
-        computed: {
-            message() {
-                return "Blank {N}-Vue app";
-            }
-        }
-    };
+
+  // import
+  import { MapView, Marker, Position } from "nativescript-google-maps-sdk";
+
+  export default {
+    data() {
+      return {
+        latitude: -33.86,
+        longitude: 151.20,
+        zoom: 8,
+        minZoom: 0,
+        maxZoom: 22,
+        bearing: 0,
+        tilt: 0,
+        mapView: undefined
+      }
+    },
+    methods: {
+      onMapReady(event) {
+        console.log("Map ready!");
+        this.mapView = event.object;
+      },
+      onCoordinateTapped() {
+        const marker = new Marker();
+        marker.position = Position.positionFromLatLng(-33.86, 151.20);
+        marker.title = "Sydney";
+        marker.snippet = "Australia";
+        marker.userData = {index: 1};
+        this.mapView.addMarker(marker);
+      }
+    }
+  };
 </script>
