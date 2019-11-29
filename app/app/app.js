ea0a31bc5c44455da5bba937c2e0dabfdeab6e99
FranciZ <franci@proxima.si>
2019-11-29 21:24:29 +0100

    Add FontIcon plugin.

--- app/app/app.js
+++ app/app/app.js
@@ -2,11 +2,19 @@ import Vue from "nativescript-vue";
 import {Mapbox} from 'nativescript-mapbox'
 import Home from "./components/Home/Home";
 import { MapView } from "nativescript-google-maps-sdk";
+import FontIcon from 'nativescript-vue-fonticon'
 
 Vue.config.silent = false;
 
 Vue.registerElement('MapView', () => MapView);
 Vue.registerElement('MapBox', () => Mapbox);
+
+Vue.use(FontIcon, {
+    debug: true, // <-- Optional. Will output the css mapping to console.
+    paths: {
+        ion: './fonts/ionicons.css'
+    }
+});
 
 new Vue({
 
