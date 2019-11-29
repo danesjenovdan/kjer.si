54e15468f224933046f9842541f6dcef078294cd
mzgajner <mato@zgajner.com>
2019-11-29 18:17:34 +0100

    Add Channel context and Category schema

---
+++ backend/lib/kjer_si/channels/category.ex
@@ -0,0 +1,19 @@
+defmodule KjerSi.Channels.Category do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+  schema "categories" do
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(category, attrs) do
+    category
+    |> cast(attrs, [:name])
+    |> validate_required([:name])
+  end
+end
