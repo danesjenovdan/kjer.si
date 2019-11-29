79fdac934ef9c1725b6b12b6d619e3ca6913d0e2
Filip Muki Dobranic <filipdobranic@gmail.com>
2019-11-29 16:36:45 +0100

    update phoenix docker thing for the last time

---
+++ backend/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+###############################################################
+#                                                             #
+# NOT BEST PRACTICE I GUESS                                   #
+# Getting dependencies after the volume was bound by          #
+# docker-compose, because it overrides any actions performed  #
+# by the Dockerfile build.                                    #
+#                                                             #
+# So if you want to have your deps available in entrypoint.sh #
+# you need to install them here as well.                      #
+#                                                             #
+###############################################################
+
+mix local.hex --force
+mix local.rebar --force
+
+mix deps.get
+
+# Create, migrate, and seed database if it doesn't exist.
+if [[ -z `psql -Atqc "\\list $PGDATABASE"` ]]; then
+  echo "Database $PGDATABASE does not exist. Creating..."
+  createdb -E UTF8 $PGDATABASE -l en_US.UTF-8 -T template0
+  mix ecto.migrate
+  mix run priv/repo/seeds.exs
+  echo "Database $PGDATABASE created."
+fi
+
+exec mix phx.server
