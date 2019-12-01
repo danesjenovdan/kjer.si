2c170559de75c946047440aa8bb8b5eafd3eebb1
FranciZ <franci@proxima.si>
2019-12-01 11:16:48 +0100

    Create map popover list.

---
+++ app/app/components/Discover/MapListPopover/MapListPopover.vue
@@ -0,0 +1,26 @@
+<template src='./MapListPopover.html'>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped src="./MapListPopover.scss" lang="scss">
+</style>
+
+<script>
+
+  import * as AppService from '../../../services/app.service';
+  import * as utils from 'tns-core-modules/utils/utils';
+  import * as ApiService from '../../../services/api.service';
+
+  export default {
+    name: 'map-card',
+    data() {
+      return {};
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    }
+  };
+</script>
