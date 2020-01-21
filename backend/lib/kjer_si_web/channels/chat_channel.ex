56d2a73f39429a73432687bfd91fca020d679d30
mzgajner <mato@zgajner.com>
2020-01-21 20:46:00 +0100

    Authenticate user when they connect to socket

--- backend/lib/kjer_si_web/channels/chat_channel.ex
+++ backend/lib/kjer_si_web/channels/chat_channel.ex
@@ -3,11 +3,10 @@ require Logger
 defmodule KjerSiWeb.ChatChannel do
   use KjerSiWeb, :channel
 
-  def join("room:" <> room_id, payload, socket) do
-    if authorized?(payload) do
-    Logger.info "joinal sobo #{room_id}"
-    send(self(), :after_join)
-    {:ok, socket}
+  def join("room:" <> room_id, _payload, socket) do
+    if authorized?(socket.assigns.user_id) do
+      send(self(), :after_join)
+      {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
@@ -22,11 +21,17 @@ defmodule KjerSiWeb.ChatChannel do
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (chat:lobby).
   def handle_in("shout", payload, socket) do
+    # Parse room ID from topic
     "room:" <> room_id = socket.topic
-    payload = Map.merge(payload, %{"room_id" => room_id})
-    KjerSi.Messages.Message.changeset(%KjerSi.Messages.Message{}, payload) |> KjerSi.Repo.insert
-    # broadcast socket, "shout", payload
 
+    # Get user ID from socket
+    user_id = socket.assigns.user_id
+
+    # Merge with incoming payload and save to DB
+    payload = Map.merge(payload, %{"room_id" => room_id, "user_id" => user_id})
+    KjerSi.Messages.Message.changeset(%KjerSi.Messages.Message{}, payload) |> KjerSi.Repo.insert
+
+    # Broadcast back to everyone in the channel
     broadcast socket, "shout", payload
 
     {:noreply, socket}
--- backend/lib/kjer_si_web/channels/user_socket.ex
+++ backend/lib/kjer_si_web/channels/user_socket.ex
@@ -17,8 +17,13 @@ defmodule KjerSiWeb.UserSocket do
   #
   # See `Phoenix.Token` documentation for examples in
   # performing token verification on connect.
-  def connect(_params, socket, _connect_info) do
-    {:ok, socket}
+  def connect(%{"user_uid" => user_uid}, socket, _connect_info) do
+    user = KjerSi.Accounts.get_user_by_uid(user_uid)
+    if user do
+      {:ok, assign(socket, :user_id, user.id)}
+    else
+      {:error, %{reason: "user does not exist"}}
+    end
   end
 
   # Socket id's are topics that allow you to identify all sockets for a given user:
