ed49aa951d61deffe07cb6485ca8d73018290a06
zigomir <zigomir@gmail.com>
2020-02-29 14:16:30 +0100

    Remove action that's not routed

--- backend/lib/kjer_si_web/controllers/map_controller.ex
+++ backend/lib/kjer_si_web/controllers/map_controller.ex
@@ -5,10 +5,6 @@ defmodule KjerSiWeb.MapController do
 
   action_fallback KjerSiWeb.FallbackController
 
-  def index(conn, _params) do
-    send_resp(conn, :no_content, "")
-  end
-
   def get_rooms_in_radius(conn, %{"lat" => lat, "lng" => lng}) do
     point = %Geo.Point{coordinates: {lng, lat}, srid: 4326}
     rooms = Rooms.get_rooms_around_point(point)
