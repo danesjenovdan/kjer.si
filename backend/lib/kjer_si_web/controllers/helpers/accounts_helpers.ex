5935b5e469386570a42f025b8835cdaac94a9867
mzgajner <mato@zgajner.com>
2020-03-08 14:09:38 +0100

    Decouple getting token so get_user can be reused

--- backend/lib/kjer_si_web/controllers/helpers/accounts_helpers.ex
+++ backend/lib/kjer_si_web/controllers/helpers/accounts_helpers.ex
@@ -3,7 +3,7 @@ defmodule KjerSi.AccountsHelpers do
 
   alias KjerSi.Accounts
 
-  defp get_token(conn) do
+  defp get_token_from_conn(conn) do
     header = conn |> get_req_header("authorization") |> to_string
     match = Regex.named_captures(~r/^Bearer (?<token>[[:ascii:]]+)/, header)
 
@@ -14,17 +14,15 @@ defmodule KjerSi.AccountsHelpers do
     end
   end
 
-  defp get_user_id(conn) do
-    with {:ok, token} <- get_token(conn) do
-      case Phoenix.Token.verify(KjerSiWeb.Endpoint, "user auth", token, max_age: 86400) do
-        {:ok, user_id} -> {:ok, user_id}
-        {:error, _} -> {:error, :invalid_token}
-      end
+  defp get_user_id(token) do
+    case Phoenix.Token.verify(KjerSiWeb.Endpoint, "user auth", token, max_age: 86400) do
+      {:ok, user_id} -> {:ok, user_id}
+      {:error, _} -> {:error, :invalid_token}
     end
   end
 
-  defp get_user(conn, preload \\ []) do
-    with {:ok, user_id} <- get_user_id(conn) do
+  def get_user(token, preload \\ []) do
+    with {:ok, user_id} <- get_user_id(token) do
       user = Accounts.get_user_with_preload(user_id, preload)
 
       if user do
@@ -35,15 +33,21 @@ defmodule KjerSi.AccountsHelpers do
     end
   end
 
+  defp get_user_from_conn(conn, preload) do
+    with {:ok, token} <- get_token_from_conn(conn) do
+      get_user(token, preload)
+    end
+  end
+
   def get_auth_user(conn, preload \\ []) do
-    case get_user(conn, preload) do
+    case get_user_from_conn(conn, preload) do
       {:ok, user} -> {:ok, user}
       {:error, error_type} -> return_error(conn, error_type)
     end
   end
 
   def is_admin(conn) do
-    case get_user(conn) do
+    case get_user_from_conn(conn, []) do
       {:ok, user} -> user.is_admin
       {:error, error_type} -> return_error(conn, error_type)
     end
