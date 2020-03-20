99b74c397db8318eacab3ac0539d92c2bf79ecab
mzgajner <mato@zgajner.com>
2020-03-20 12:10:27 +0100

    Run `mix format` on files that will be edited

--- backend/test/test_helper.exs
+++ backend/test/test_helper.exs
@@ -5,29 +5,36 @@ defmodule TestHelper do
   use KjerSiWeb.ConnCase
 
   def generate_category do
-    {:ok, category} = KjerSi.Rooms.create_category(%{
-      name: "Test category",
-    })
+    {:ok, category} =
+      KjerSi.Rooms.create_category(%{
+        name: "Test category"
+      })
+
     category
   end
 
   def generate_user do
-    {:ok, user} = KjerSi.Accounts.create_user(%{
-      nickname: "TestNickname",
-      uid: "1337",
-    })
+    {:ok, user} =
+      KjerSi.Accounts.create_user(%{
+        nickname: "TestNickname",
+        uid: "1337"
+      })
+
     user
   end
 
   def generate_room do
     test_category = generate_category()
-    {:ok, room} = KjerSi.Rooms.create_room(%{
-      lat: 120.5,
-      lng: 120.5,
-      name: "some name",
-      radius: 42,
-      category_id: test_category.id,
-    })
+
+    {:ok, room} =
+      KjerSi.Rooms.create_room(%{
+        lat: 120.5,
+        lng: 120.5,
+        name: "some name",
+        radius: 42,
+        category_id: test_category.id
+      })
+
     room
   end
 
