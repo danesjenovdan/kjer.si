6e2206ac404db18d844820f951874ebd8aa71788
mzgajner <mato@zgajner.com>
2020-03-22 13:54:38 +0100

    Configure formatter for params module

--- backend/.formatter.exs
+++ backend/.formatter.exs
@@ -1,4 +1,8 @@
 [
+  locals_without_parens: [
+    # Params
+    defparams: 1,
+  ],
   import_deps: [:ecto, :phoenix],
   inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
   subdirectories: ["priv/*/migrations"]
