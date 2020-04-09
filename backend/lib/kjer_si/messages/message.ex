ac4d0ffad7eee0497cdf00c4d7097d4142c6428a
mzgajner <mato@zgajner.com>
2020-04-09 14:13:37 +0200

    Don't send channel history over socket after joining

--- backend/lib/kjer_si/messages/message.ex
+++ backend/lib/kjer_si/messages/message.ex
@@ -1,7 +1,6 @@
 defmodule KjerSi.Messages.Message do
   use Ecto.Schema
   import Ecto.Changeset
-  import Ecto.Query
 
   @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id
@@ -18,15 +17,5 @@ defmodule KjerSi.Messages.Message do
     message
     |> cast(attrs, [:content, :room_id, :user_id])
     |> validate_required([:content, :room_id, :user_id])
-  end
-
-  def get_messages_for_room(room_id) do
-    query =
-      from m in KjerSi.Messages.Message,
-        where: m.room_id == ^room_id,
-        order_by: [asc: m.inserted_at],
-        preload: [:user]
-
-    KjerSi.Repo.all(query)
   end
 end
