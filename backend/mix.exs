036f169b5372040d13ad0578f561b741d726e58b
muki <filipdobranic@gmail.com>
2021-03-19 08:57:32 +0100

    bump deps

--- backend/mix.exs
+++ backend/mix.exs
@@ -33,17 +33,17 @@ defmodule KjerSi.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.1"},
-      {:ecto_sql, "~> 3.4"},
+      {:phoenix, "~> 1.5"},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:phoenix_ecto, "~> 4.2"},
+      {:ecto_sql, "~> 3.5"},
       {:postgrex, "~> 0.15"},
-      {:gettext, "~> 0.17"},
+      {:gettext, "~> 0.18"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.1"},
+      {:plug_cowboy, "~> 2.4"},
       {:geo, "~> 3.3"},
       {:geo_postgis, "~> 3.3"},
-      {:params, "~> 2.1"}
+      {:params, "~> 2.2"}
     ]
   end
 
